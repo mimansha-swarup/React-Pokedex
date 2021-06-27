184e896a6a055389eb1f8402eb003abc1ff9a6d3
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-06-27 11:36:05 +0530

    refactor key given

---
+++ src/Components/Pokemon/SpritesCard.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import {PokeContext} from "../../Context/PokeContext"
+
+
+const SpritesCard = () => {
+    const {pokeData} =useContext(PokeContext)
+  
+    return ( 
+        <div className="sprite-card">
+            
+            
+          </div>
+     );
+}
+ 
+export default SpritesCard;
